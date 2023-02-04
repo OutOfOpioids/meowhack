13b0a4daf070b918c4dc1ef5214ea0c9c3347389
jan <jan.kuczaj@gmail.com>
2023-02-04 12:07:00 +0100

    New Watermark

--- src/main/java/me/notkronos/meowhack/Meowhack.java
+++ src/main/java/me/notkronos/meowhack/Meowhack.java
@@ -21,7 +21,7 @@ import java.util.function.Predicate;
 public class Meowhack {
     public static final String MODID = "meowhack";
     public static final String NAME = "Meowhack";
-    public static final String VERSION = "1.1+6c1471d4";
+    public static final String VERSION = "1.1+a497655e";
     public static boolean SETUP = false;
     public static String PREFIX = "++";
 
