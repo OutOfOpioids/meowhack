35e43d01187948dba049b5ef20548c0da488344f
jan <jan.kuczaj@gmail.com>
2023-02-07 18:14:35 +0100

    Meowhack 1.3.1

--- src/main/java/me/notkronos/meowhack/Meowhack.java
+++ src/main/java/me/notkronos/meowhack/Meowhack.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Meowhack {
     public static final String MODID = "meowhack";
     public static final String NAME = "Meowhack";
-    public static final String VERSION = "1.3+e2f62d7c";
+    public static final String VERSION = "1.3.1";
     public static boolean SETUP = false;
     public static String PREFIX = "++";
 
