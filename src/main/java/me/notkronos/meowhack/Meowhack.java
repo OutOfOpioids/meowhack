1361c171652faf1e20e3fe502c818bc7506ff7e5
jan <jan.kuczaj@gmail.com>
2023-02-13 12:59:46 +0100

    added Delay setting to AutoMeow

--- src/main/java/me/notkronos/meowhack/Meowhack.java
+++ src/main/java/me/notkronos/meowhack/Meowhack.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Meowhack {
     public static final String MODID = "meowhack";
     public static final String NAME = "Meowhack";
-    public static final String VERSION = "1.3.1+7f8b074c";
+    public static final String VERSION = "1.3.1+d4d4c9bb";
     public static boolean SETUP = false;
     public static String PREFIX = "++";
 
--- src/main/java/me/notkronos/meowhack/module/misc/AutoMeow.java
+++ src/main/java/me/notkronos/meowhack/module/misc/AutoMeow.java
@@ -3,6 +3,7 @@ package me.notkronos.meowhack.module.misc;
 
 import me.notkronos.meowhack.module.Category;
 import me.notkronos.meowhack.module.Module;
+import me.notkronos.meowhack.setting.Setting;
 
 import static me.notkronos.meowhack.util.Wrapper.mc;
 
@@ -10,7 +11,6 @@ import static me.notkronos.meowhack.util.Wrapper.mc;
 public class AutoMeow extends Module {
     public static AutoMeow INSTANCE;
     private long lastMeow = 0;
-
 
     public AutoMeow() {
         super("AutoMeow", Category.MISC, "Meows so you don't have to!", new String[]{});
@@ -20,12 +20,14 @@ public class AutoMeow extends Module {
 
     }
 
+    public static Setting<Integer> seconds = new Setting<>("Delay", 30, 1, 60);
+
     @Override
     public void onTick() {
         if (isEnabled()) {
-            int time = 30; // set time in seconds
+            int time = seconds.getValue(); // set time in seconds
             long tick = mc.world.getTotalWorldTime();
-            if (tick - lastMeow >= 20 * time) {           // stops it from meowing twice
+            if (tick - lastMeow >= 20L * time) {           // stops it from meowing twice
                 mc.player.sendChatMessage("meow");
                 lastMeow = tick;
             }
