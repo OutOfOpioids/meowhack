bb83f5e512662c96df88c957985d3d7f258bf324
C10H15NO <C10H15NO@teczowysekai.live>
2023-08-21 01:24:53 +0200

    Fixed StrengthDetect

--- src/main/java/me/notkronos/meowhack/module/combat/StrengthDetect.java
+++ src/main/java/me/notkronos/meowhack/module/combat/StrengthDetect.java
@@ -1,11 +1,15 @@
 package me.notkronos.meowhack.module.combat;
 
+import me.notkronos.meowhack.Meowhack;
 import me.notkronos.meowhack.module.Category;
 import me.notkronos.meowhack.module.Module;
 import me.notkronos.meowhack.util.chat.MessageSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.potion.PotionEffect;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
 import java.util.WeakHashMap;
@@ -19,22 +23,23 @@ public class StrengthDetect extends Module {
     private final MessageSender messageSender = new MessageSender();
 
     public StrengthDetect() {
-        super("StrengthNotif", Category.COMBAT, "Notifies when a gay person is detected in your visualrange", new String[]{});
+        super("StrengthNotif", Category.COMBAT, "Notifies when someone gets strength in your visual range", new String[]{});
         INSTANCE = this;
         this.enabled = false;
         this.drawn = false;
     }
 
     @Override
-    public void onUpdate() {
+    public void onTick() {
         for (EntityPlayer player : mc.world.playerEntities) {
-            if (player.equals(mc.player)) continue;
-            if (player.isPotionActive(MobEffects.STRENGTH) && !this.str.contains(player)) {
-                messageSender.sendMessageClientSide(player.getDisplayNameString() + " has strength");
-                this.str.add(player);
+            if(player.isPotionActive(MobEffects.STRENGTH) && !player.equals(mc.player)) {
+                if (!str.contains(player)) {
+                    str.add(player);
+                    messageSender.sendMessageClientSide(player.getName() + " has strength!");
+                }
+            } else {
+                str.remove(player);
             }
-            if (this.str.contains(player) || player.isPotionActive(MobEffects.STRENGTH)) continue;
-            this.str.remove(player);
         }
     }
 }
