e930c519d26504acbe1c2b295c3af049e93d9be6
NotKronos <jan.kuczaj@gmail.com>
2023-08-31 13:29:22 +0200

    Added settings to DeathEffects

--- src/main/java/me/notkronos/meowhack/module/render/DeathEffects.java
+++ src/main/java/me/notkronos/meowhack/module/render/DeathEffects.java
@@ -4,6 +4,7 @@ import me.notkronos.meowhack.Meowhack;
 import me.notkronos.meowhack.event.events.entity.DeathEvent;
 import me.notkronos.meowhack.module.Category;
 import me.notkronos.meowhack.module.Module;
+import me.notkronos.meowhack.setting.Setting;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.client.renderer.entity.RenderLightningBolt;
@@ -24,13 +25,20 @@ public class DeathEffects extends Module {
         Meowhack.EVENT_BUS.register(this);
     }
 
+    public Setting<Boolean> lightning = new Setting<>("Lightning", true);
+    public Setting<Boolean> sound = new Setting<>("Sound", true);
+
     @SubscribeEvent
     public void onDeathEvent(DeathEvent event) {
         if (isEnabled() && event.getEntity() instanceof EntityOtherPlayerMP) {
             EntityLightningBolt lightningBoltEntity = new EntityLightningBolt(mc.world, event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ, true);
-            mc.world.addWeatherEffect(lightningBoltEntity);
-            mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_LIGHTNING_IMPACT, 1.0F));
-            mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_LIGHTNING_THUNDER, 1.0F));
+            if(lightning.getValue()) {
+                mc.world.addWeatherEffect(lightningBoltEntity);
+            }
+            if(sound.getValue()) {
+                mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_LIGHTNING_IMPACT, 1.0F));
+                mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_LIGHTNING_THUNDER, 1.0F));
+            }
         }
     }
 }
