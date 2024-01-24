1d7f444fd40c6138c0f3903231c077c3d2ed78c3
NotKronos <jan.kuczaj@gmail.com>
2024-01-24 15:14:12 +0100

    Added an error message when trying to set a font that doesn't exist

--- src/main/java/me/notkronos/meowhack/command/commands/Font.java
+++ src/main/java/me/notkronos/meowhack/command/commands/Font.java
@@ -4,9 +4,14 @@ import me.notkronos.meowhack.Meowhack;
 import me.notkronos.meowhack.command.Command;
 import me.notkronos.meowhack.font.CustomFontRenderer;
 import me.notkronos.meowhack.module.client.CustomFontMod;
+import me.notkronos.meowhack.util.chat.ChatUtil;
+import me.notkronos.meowhack.util.chat.MessageType;
 import me.notkronos.meowhack.util.render.FontUtil;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Font extends Command {
     public Font() {
@@ -18,16 +23,21 @@ public class Font extends Command {
     @Override
     public void onExecute(String[] args) throws IOException {
         if (args.length == 1) {
-            FontUtil.customFont = new CustomFontRenderer(
-                    new java.awt.Font(args[0],
-                            CustomFontMod.fontStyle.value,
-                            CustomFontMod.fontSize.value
-                    ),
-                    CustomFontMod.antiAlias.value,
-                    CustomFontMod.metrics.value
-            );
-            fontName = args[0];
-            Meowhack.INSTANCE.getConfigManager().saveFont(args[0]);
+            List<String> fonts = Arrays.asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
+            if(fonts.stream().anyMatch(f -> f.equalsIgnoreCase(args[0]))) {
+                FontUtil.customFont = new CustomFontRenderer(
+                        new java.awt.Font(args[0],
+                                CustomFontMod.fontStyle.value,
+                                CustomFontMod.fontSize.value
+                        ),
+                        CustomFontMod.antiAlias.value,
+                        CustomFontMod.metrics.value
+                );
+                fontName = args[0];
+                Meowhack.INSTANCE.getConfigManager().saveFont(args[0]);
+            } else {
+                ChatUtil.commandFeedback("Font " + args[0] + " not found.", MessageType.ERROR);
+            }
         }
     }
 
